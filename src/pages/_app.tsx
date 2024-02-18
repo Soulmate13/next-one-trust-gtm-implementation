7dfe674921dce77d69b581a8f2cb0e3aeb2e3372
Hlib Zhurba <glebzhurba@gmail.com>
2024-02-18 13:05:19 +0200

    added gtm and cookiebot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+        <Script 
+        id="Cookiebot" 
+        src="https://consent.cookiebot.com/uc.js" 
+        data-cbid="3220e69b-f1a4-4a26-8638-7d1c815fe5b3" 
+        data-blockingmode="auto" 
+        type="text/javascript"
+        />
+
+      <Script 
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T3S7HCS9');`
+          }}
+/>
+  <Component {...pageProps} />
+  </>)
+
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
+
       <body>
+
         <Main />
         <NextScript />
       </body>
